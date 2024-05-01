fc84a1d8e1918e2bb523377d0aa02ce1d165be38
Gabotw <u202018627@upc.edu.pe>
2024-05-01 16:30:47 -0500

    feat: update lawyer api service client.

--- src/lawyer/services/lawyer-api.service.js
+++ src/lawyer/services/lawyer-api.service.js
@@ -1,28 +1,8 @@
 import http from "../../shared/services/http-common.js";
 
 export class lawyerApiService {
-    getAll() {
-        return http.get("/consultation/lawyers");
-    }
-
-    get(id) {
-        return http.get(`/lawyers/${id}`);
-    }
-
-    create(data) {
-        return http.post("/lawyers", data);
-    }
-
-    update(id, data) {
-        return http.put(`/lawyers/${id}`, data);
-    }
-
-    delete(id) {
-        return http.delete(`/lawyers/${id}`);
-    }
-
-    deleteAll() {
-        return http.delete(`/lawyers`);
+    static getAll() {
+        return http.get("/lawyers");
     }
 
     findByTitle(title) {

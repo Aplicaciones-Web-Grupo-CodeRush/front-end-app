932e70dc2fec1ec79242f238c924eaf45d6ece85
Fabricio Apaza Morales <u201922146@upc.edu.pe>
2024-06-21 08:56:34 -0500

    feat(sign-up):  added sign-up component.

---
+++ src/public/pages/sign-up/sign-up.component.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  name: 'sign-up',
+  data() {
+    return {
+      email: '',
+      password: '',
+      confirmPassword: ''
+    }
+  },
+  methods: {
+    goToLogin() {
+      this.$router.push('/signin');
+    },
+    signUp() {
+      // Aquí puedes agregar la lógica de autenticación
+      // Si la autenticación es exitosa, redirige a la página de abogados
+      this.$router.push('/abogados');
+    }
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
